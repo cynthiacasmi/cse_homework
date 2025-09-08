1d3370de7712376cd7404b1710669e71e345f626
Liz Wolf <cynthiacasmi@gmail.com>
2025-09-07 22:35:00 -0600

    testing fetching headers

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const path =  require("path");
 
 const app = express();
-app.use(express.static("public")); //serving files from public folder
+app.use("/public", express.static(path.join(__dirname + "/public"))); //serving files from public folder
                                   // In that folder I can place everything for my website
+
+app.get("/api", (req, res) => {
+  res.json("HTTP GET request received");
+})
+
+app.use((req, res) => {
+  //res.status(404);
+  //res.send("<h1>Error 404: Resource Not found</h1>")
+  res.send(JSON.stringify(req.rawHeaders));
+})
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
