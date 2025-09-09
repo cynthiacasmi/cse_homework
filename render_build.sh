714b1c385177bc3d81ba590aa265aa516843bc5a
Liz Wolf <cynthiacasmi@gmail.com>
2025-09-08 19:41:59 -0600

    bash file for tunnel dependencies

---
+++ render_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install app.js dependency for Node.js
+npm install express
+
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
+
+sudo cloudflared service install eyJhIjoiYWViYTM5YWQ0MmRiMzJhOWJjYzQ3NTA1OTkzZTMwZDUiLCJ0IjoiYjE0ODlmYzYtMDVhNC00ZDRhLTgzNjEtNzgzNTU1M2NlZTJiIiwicyI6Ik5qUTJObUl4TW1RdE5EWmhZUzAwWldJeExUZzJOV1V0WWpGak56bGtOakJtTVRVMSJ9
