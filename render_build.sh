41c1bf4b3baf3da503e16367bcd98f23e1f45416
Liz Wolf <cynthiacasmi@gmail.com>
2025-09-08 20:38:21 -0600

    Change index.html content and tried Source Build cloudflared

--- render_build.sh
+++ render_build.sh
@@ -3,14 +3,7 @@
 # Install app.js dependency for Node.js
 npm install express
 
-# Add cloudflare gpg key
-mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
-
-# Add this repo to your apt repositories
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | tee /etc/apt/sources.list.d/cloudflared.list
-
-# install cloudflared
-apt-get update && apt-get install cloudflared
-
-cloudflared service install eyJhIjoiYWViYTM5YWQ0MmRiMzJhOWJjYzQ3NTA1OTkzZTMwZDUiLCJ0IjoiYjE0ODlmYzYtMDVhNC00ZDRhLTgzNjEtNzgzNTU1M2NlZTJiIiwicyI6Ik5qUTJObUl4TW1RdE5EWmhZUzAwWldJeExUZzJOV1V0WWpGak56bGtOakJtTVRVMSJ9
+git clone https://github.com/cloudflare/cloudflared.git
+cd cloudflared
+make cloudflared
+go install github.com/cloudflare/cloudflared/cmd/cloudflared
