4ea1564833bd9e0985478fb3094cf585f454c4cf
Liz Wolf <cynthiacasmi@gmail.com>
2025-09-08 20:50:27 -0600

    Deleted all in render_build except for the express install

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,3 @@
 
 # Install app.js dependency for Node.js
 npm install express
-
-git clone https://github.com/cloudflare/cloudflared.git
-cd cloudflared
-make cloudflared
-apt install golang
-go install github.com/cloudflare/cloudflared/cmd/cloudflared
