09e44e0aec7e5431679c6acd2c3ba4fa1234daf7
Liz Wolf <cynthiacasmi@gmail.com>
2025-09-08 20:43:32 -0600

    Added golang install

--- render_build.sh
+++ render_build.sh
@@ -6,4 +6,5 @@ npm install express
 git clone https://github.com/cloudflare/cloudflared.git
 cd cloudflared
 make cloudflared
+apt install golang
 go install github.com/cloudflare/cloudflared/cmd/cloudflared
